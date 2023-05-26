d05d7cd64ff250093cd7193f0d9ae64f272036d2
fjmg84 <fjmgqba@gmail.com>
2023-05-26 18:30:49 -0400

    create banner

---
+++ component/Banner/index.js
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+
+function Banner() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.slider}>
+        <div>
+          <Image
+            src="/recursos/main/slider-1.jpg"
+            width={768}
+            height={518}
+            alt="slider 1"
+            className={styles.slider}
+          />
+          <div>
+            <h1>jumpsuits</h1>
+            <h4>Comfortable clothes for your babies</h4>
+            <button>
+              <span>discover</span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/recursos/main/girls-power.jpg"
+            width={340}
+            height={438}
+            alt="slider 2"
+            className={styles.girlsPower}
+          />
+          <div>
+            <h1>girl power</h1>
+            <h4>For a colorful summer</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.categories}>
+        <div>
+          <Image
+            src="/recursos/main/baby.png"
+            width={150}
+            height={180}
+            alt="Baby & Toddler"
+            className={styles.slider}
+          />
+          <h5>Baby & Toddler</h5>
+        </div>
+        <div>
+          <Image
+            src="/recursos/main/girl.png"
+            width={150}
+            height={180}
+            alt="For Girls"
+            className={styles.slider}
+          />
+          <h5>For Girls</h5>
+        </div>
+        <div>
+          <Image
+            src="/recursos/main/boy.png"
+            width={150}
+            height={180}
+            alt="For Boys"
+            className={styles.slider}
+          />
+          <h5>For Boys</h5>
+        </div>
+        <div>
+          <Image
+            src="/recursos/main/toys.png"
+            width={150}
+            height={180}
+            alt="Home & Toys"
+            className={styles.slider}
+          />
+          <h5>Home & Toys</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import styles from '../pages/index.module.css'
+import styles from "../pages/index.module.css";
+import Banner from "../component/Banner";
 
 export default function Home() {
   return (
@@ -8,9 +9,14 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
       </Head>
+      <div className={styles.container_banner}>
+        <Banner />
+      </div>
     </div>
-  )
+  );
 }
