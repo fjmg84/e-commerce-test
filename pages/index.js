7f5b8fd2b6205d62add89f9e06ee6d24c1bcd9d0
fjmg84 <fjmgqba@gmail.com>
2023-05-26 22:01:51 -0400

    add data by StaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,43 @@
-import styles from "../pages/index.module.css";
+import Head from "next/head";
 import Banner from "../component/Banner";
+import mockData from "../mock/data.json";
+import styles from "../pages/index.module.css";
 
-export default function Home() {
+export default function Home({ categories, products }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.container_banner}>
-        <Banner />
+    <>
+      <Head>
+        <title>E-Commerce Baby</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.container_banner}>
+          <Banner />
+        </div>
       </div>
-    </div>
+    </>
   );
+}
+
+export async function getStaticProps() {
+  let categories = [],
+    temp = [];
+  mockData.forEach((arr) => {
+    let nameCategory = arr.category;
+    if (temp.includes(nameCategory) === false) {
+      let countCategory = mockData.filter(
+        (arr) => arr.category === nameCategory
+      ).length;
+      temp = [...temp, nameCategory];
+      categories = [
+        ...categories,
+        { name: nameCategory, count: countCategory },
+      ];
+    }
+  });
+  return {
+    props: {
+      categories,
+      products: mockData,
+    },
+  };
 }
