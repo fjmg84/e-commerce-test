184a75e781b04ccd3945505f1884573e7ed76799
fjmg84 <fjmgqba@gmail.com>
2023-05-31 21:16:51 -0400

    create BestProduct component

---
+++ component/Product/Best/index.js
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import Image from "next/image";
+import useRate from "../../../hooks/useRate";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+function BestProducts({ products }) {
+  const { generated, rates } = useRate();
+
+  useEffect(() => {
+    generated({
+      value: products[0].rate,
+      colorFill: styles.fill,
+      colorOutFill: styles.out_fill,
+    });
+  }, [products]);
+  return (
+    <div className={styles.container}>
+      <h1>Best Sellers</h1>
+      <div className={styles.container_list}>
+        {products.map((product) => {
+          return (
+            <div key={product.sku} className={styles.box}>
+              <div className={styles.box_image}>
+                <Image
+                  src={product.images[0]}
+                  width={50}
+                  height={50}
+                  alt={product.images[0]}
+                />
+              </div>
+
+              <div className={styles.box_desc}>
+                <h4>
+                  <Link
+                    href={{
+                      pathname: "/[category]/[title]",
+                      query: {
+                        category: product.category.replaceAll(" ", "_"),
+                        title: product.title.replaceAll(" ", "_"),
+                      },
+                    }}
+                  >
+                    {product.title}
+                  </Link>
+                </h4>
+                <h4>
+                  {rates.map((rate, index) => (
+                    <i key={index} className={`${rate}`}></i>
+                  ))}
+                </h4>
+                <h4>{`$${product.price}.00`}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default BestProducts;
